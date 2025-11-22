1318bff22393818789c6903a45dae63aa6ea181a
Varshith730 <127433449+Varshith730@users.noreply.github.com>
2025-11-23 00:58:06 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,15 +3,11 @@ import zipfile
 import os
 from functools import lru_cache
 
-
 DATA_DIR = "data"
 DELIVERY_CSV = f"{DATA_DIR}/deliveries.csv"
 DELIVERY_ZIP = f"{DATA_DIR}/deliveries.csv.zip"
 
 
-# -------------------------------
-# Load Matches
-# -------------------------------
 @lru_cache(maxsize=1)
 def load_matches(path=f"{DATA_DIR}/matches.csv"):
     df = pd.read_csv(path)
@@ -19,9 +15,6 @@ def load_matches(path=f"{DATA_DIR}/matches.csv"):
     return df
 
 
-# -------------------------------
-# Load Lifetime Dataset
-# -------------------------------
 @lru_cache(maxsize=1)
 def load_lifetime(path=f"{DATA_DIR}/cricket_data.csv"):
     df = pd.read_csv(path)
@@ -29,24 +22,20 @@ def load_lifetime(path=f"{DATA_DIR}/cricket_data.csv"):
     return df
 
 
-# -------------------------------------
-# LOAD DELIVERIES (Auto-unzip supported)
-# -------------------------------------
 @lru_cache(maxsize=1)
 def load_deliveries():
 
-    # If CSV already extracted → just load it
+    # If CSV already extracted → load it
     if os.path.exists(DELIVERY_CSV):
         df = pd.read_csv(DELIVERY_CSV)
         df.columns = [c.strip() for c in df.columns]
         return df
 
-    # If CSV missing → but ZIP exists → extract it
+    # If ZIP exists → extract deliveries.csv
     elif os.path.exists(DELIVERY_ZIP):
         try:
             with zipfile.ZipFile(DELIVERY_ZIP, "r") as z:
                 z.extractall(DATA_DIR)
-            print("✔ Extracted deliveries.csv from ZIP")
 
             df = pd.read_csv(DELIVERY_CSV)
             df.columns = [c.strip() for c in df.columns]
@@ -57,5 +46,5 @@ def load_deliveries():
 
     else:
         raise FileNotFoundError(
-            "❌ deliveries.csv NOT found and deliveries.csv.zip NOT found in /data folder"
+            "deliveries.csv NOT found and deliveries.csv.zip NOT found inside /data"
         )
